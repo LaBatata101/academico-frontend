fa78eb8f54cf93bce583dc66695e0e9b85de4a68
LaBatata101 <labatata101@linuxmail.org>
2022-12-04 17:36:41 -0300

    get data from API to populate the list

--- src/pages/StudentList.tsx
+++ src/pages/StudentList.tsx
@@ -3,18 +3,50 @@ import { StudentList, StudentListItem } from "../components/List";
 import AddIcon from "@mui/icons-material/Add";
 import { Page } from "../components/Page";
 import React from "react";
+import { getUrl, Student } from "../api";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export const StudentListPage = () => {
+    const [students, setStudents] = React.useState([]);
+    const handleFetchStudents = React.useCallback(() => {
+        axios
+            .get(getUrl("/student", "/"))
+            .then((result) => setStudents(result.data))
+            .catch((error) => console.log(error));
+    }, []);
+
+    React.useEffect(() => {
+        handleFetchStudents();
+    }, [handleFetchStudents]);
+
     return (
         <>
             <Page
                 header="Lista de Alunos"
-                button=<Button sx={{ flex: "2 2 10%" }} variant="contained" startIcon={<AddIcon />}>
+                button=<Button
+                    sx={{ flex: "2 2 10%" }}
+                    variant="contained"
+                    startIcon={<AddIcon />}
+                    component={Link}
+                    to="/alunos/cadastrar"
+                >
                     Novo Aluno
                 </Button>
             >
                 <StudentList>
-                    <StudentListItem />
+                    {students.map((student: Student) => (
+                        <StudentListItem
+                            key={student.name}
+                            student={student}
+                            onEdit={(student) => {
+                                console.log(JSON.stringify(student));
+                            }}
+                            onDelete={(student) => {
+                                console.log(JSON.stringify(student));
+                            }}
+                        />
+                    ))}
                 </StudentList>
             </Page>
         </>
