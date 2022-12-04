f89fe88ab12c09121a387840b9fa22099190fd18
LaBatata101 <labatata101@linuxmail.org>
2022-12-04 17:32:10 -0300

    add API types (Student and Discipline) and method to build API URL

---
+++ src/api/index.ts
@@ -0,0 +1,22 @@
+export const API_BASE = "http://localhost:8080";
+
+export const getUrl = (endpoint: "/" | "/student", path: string, url_parameter?: string) => {
+    let url = `${API_BASE}${endpoint}${path}`;
+    if (url_parameter) {
+        url = `${url}/${url_parameter}`;
+    }
+    return url;
+};
+
+export type Discipline = {
+    name: string;
+    workload: number;
+};
+
+export type Student = {
+    name: string;
+    email: string;
+    phone: string;
+    birthday: Date;
+    disciplines: Array<Discipline>;
+};
