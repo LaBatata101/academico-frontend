442f08e340b0d354681bc542c31efde0ddf5af43
LaBatata101 <labatata101@linuxmail.org>
2022-12-04 17:26:27 -0300

    implement submenus to sidemenu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,23 +1,86 @@
 import {
     Box,
+    Collapse,
     Drawer,
     List,
     ListItem,
     ListItemButton,
     ListItemIcon,
     ListItemText,
+    SxProps,
     Toolbar,
 } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { Theme } from "pretty-format";
 
 type MenuItem = {
     label: string;
-    href: string;
+    href?: string;
     icon?: React.ReactNode;
+    submenus?: Array<MenuItem>;
 };
 
 type SideMenuProps = { menu_items: Array<MenuItem> };
+
+const SideMenuItem = ({
+    href,
+    icon,
+    label,
+    sx,
+}: {
+    href?: string;
+    icon?: React.ReactNode;
+    label: string;
+    sx?: SxProps<Theme>;
+}) => (
+    <ListItem>
+        {href ? (
+            <ListItemButton component={Link} to={href}>
+                {icon !== undefined && <ListItemIcon>{icon}</ListItemIcon>}
+                <ListItemText primary={label} />
+            </ListItemButton>
+        ) : (
+            <ListItemButton>
+                {icon !== undefined && <ListItemIcon>{icon}</ListItemIcon>}
+                <ListItemText primary={label} />
+            </ListItemButton>
+        )}
+    </ListItem>
+);
+
+const SideMenuItemWithSubmenus = ({ menu_item }: { menu_item: MenuItem }) => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <>
+            <ListItemButton onClick={handleClick}>
+                <ListItemIcon>{menu_item.icon}</ListItemIcon>
+                <ListItemText primary={menu_item.label} />
+                {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    {menu_item.submenus &&
+                        menu_item.submenus.map((menu_item) => (
+                            <SideMenuItem
+                                sx={{ pl: 4 }}
+                                label={menu_item.label}
+                                href={menu_item.href}
+                                icon={menu_item.icon}
+                            />
+                        ))}
+                </List>
+            </Collapse>
+        </>
+    );
+};
 
 export const SideMenu = ({ menu_items }: SideMenuProps) => {
     return (
@@ -34,16 +97,20 @@ export const SideMenu = ({ menu_items }: SideMenuProps) => {
             <Toolbar />
             <Box sx={{ overflow: "auto" }}>
                 <List>
-                    {menu_items.map((menu_item: MenuItem, index: number) => (
-                        <ListItem key={`${menu_item.label}_${index}`}>
-                            <ListItemButton component={Link} to={menu_item.href}>
-                                {menu_item.icon !== undefined && (
-                                    <ListItemIcon>{menu_item.icon}</ListItemIcon>
-                                )}
-                                <ListItemText primary={menu_item.label} />
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
+                    {menu_items.map((menu_item: MenuItem, index: number) => {
+                        if (menu_item.submenus) {
+                            return <SideMenuItemWithSubmenus menu_item={menu_item} />;
+                        } else {
+                            return (
+                                <SideMenuItem
+                                    key={`${menu_item.label}_${index}`}
+                                    href={menu_item.href}
+                                    icon={menu_item.icon}
+                                    label={menu_item.label}
+                                />
+                            );
+                        }
+                    })}
                 </List>
             </Box>
         </Drawer>
