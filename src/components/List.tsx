8bea9a4b080ad1f78bcb7628889e1ecd519b4626
LaBatata101 <labatata101@linuxmail.org>
2022-12-20 17:18:23 -0300

    update List component to allow any text in the header instead of a hard coded one

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -15,42 +15,45 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import React from "react";
 import { Student } from "../api";
 
-export const StudentList = ({ children }: { children: React.ReactNode }) => {
+export const List = ({
+    column_headers,
+    children,
+}: {
+    column_headers: React.ReactNode[];
+    children: React.ReactNode;
+}) => {
     return (
         <TableContainer component={Paper} sx={{ mt: 1 }}>
             <Table sx={{ minWidth: 650 }}>
-                <TableHead>
-                    <TableCell>#</TableCell>
-                    <TableCell align="left">Nome do Aluno</TableCell>
-                    <TableCell align="center">Ação</TableCell>
-                </TableHead>
+                <TableHead>{column_headers}</TableHead>
                 <TableBody>{children}</TableBody>
             </Table>
         </TableContainer>
     );
 };
 
-export const StudentListItem = ({
-    student,
+export const ListItem = ({
     onEdit,
     onDelete,
+    children,
+    object,
 }: {
-    student: Student;
-    onEdit: (student: Student) => void;
-    onDelete: (student: Student) => void;
+    children: React.ReactNode;
+    object: any;
+    onEdit: () => void;
+    onDelete: () => void;
 }) => {
     return (
         <TableRow>
-            <TableCell>0</TableCell>
-            <TableCell align="left">{student.name}</TableCell>
+            {children}
             <TableCell align="center">
                 <Tooltip title={<Typography fontSize={14}>Editar</Typography>} arrow>
-                    <IconButton color="info" onClick={() => onEdit(student)}>
+                    <IconButton color="info" onClick={onEdit}>
                         <EditIcon />
                     </IconButton>
                 </Tooltip>
                 <Tooltip title={<Typography fontSize={14}>Deletar</Typography>} arrow>
-                    <IconButton color="error" onClick={() => onDelete(student)}>
+                    <IconButton color="error" onClick={onDelete}>
                         <DeleteIcon />
                     </IconButton>
                 </Tooltip>
