b7f3479be005b84206eaeae415ea17bf3ec2aebe
LaBatata101 <labatata101@linuxmail.org>
2022-12-20 17:16:18 -0300

    add more API endpoints

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,14 @@
 export const API_BASE = "http://localhost:8080";
 
 export const getUrl = (
-    endpoint: "/" | "/student" | "/discipline" | "/professor",
+    endpoint:
+        | "/"
+        | "/student"
+        | "/discipline"
+        | "/professor"
+        | "/class"
+        | "/course"
+        | "/curriculum",
     path: string,
     url_parameter?: string
 ) => {
